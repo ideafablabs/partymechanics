a62e986edc2a7da9592362322a7690c1b6339c84
Jordan Layman <jordanmlayman@gmail.com>
2020-02-21 04:44:46 -0800

    ignore config.h

--- Arduino/vortex_cannon/config.h
+++ Arduino/vortex_cannon/config.h
@@ -6,32 +6,31 @@
  */
 
 #define ZONE_ID 1 // Zone ID's can be found on the website.
-#define READER_ID 0 // 0 for Zone, 1 for intake.
-const String DNS_NAME = "vortexrings"; // Zone DNS
+#define READER_ID 2 // 0 for Zone, 1 for intake.
+const String DNS_NAME = "nfc-reader-2"; // Zone DNS
 
-//  NFC
-#define READER_COUNT 1
-
-#define SSID1 "omino warp"
-#define PASSWORD1 "0123456789"
+#define SSID1 "loadingdockap"
+#define PASSWORD1 "Ldock55AP$securityKEY*"
 #define SSID2 "Idea Fab Labs"
 #define PASSWORD2 "vortexrings"
+// #define SSID3 "themint"
+// #define PASSWORD1 "vortexrings"
 
-// const String API_BASE = "http://10.0.4.127/"; //Temporary Local
 // const String API_BASE = "http://192.168.0.72/"; //Temporary Local
-const String API_BASE = "https://mint.ideafablabs.com/"; //Live
-// const String API_BASE = "https://santacruz.ideafablabs.com/"; //Live
+const String API_BASE = "http://mint.ideafablabs.com/"; //Live
+// const String API_BASE = "http://santacruz.ideafablabs.com/"; //Live
 
+// const String API_ENDPOINT = "wp-json/zoneplusone/v1/";
 const String API_ENDPOINT = "wp-json/mint/v1/";
-const String LOG_FILE = "/actions.log";
+
+#define LOG_FILE "/actions.log"
 
 // LED Details
-#define LEDPIN 5
+#define LEDPIN 2
 #define NUM_LEDS 16
 #define BRIGHTNESS 100
 
 // NFC Details
-#define TOKENMAX 4
 
 // LOLIN MCU
 // #define PN532_SCK  14	// SK
@@ -42,9 +41,8 @@ const String LOG_FILE = "/actions.log";
 // WEMOS D1
 #define PN532_SCK  14
 #define PN532_MOSI 13
+#define PN532_SS   15
 #define PN532_MISO 12
-#define PN532_SS  15
-// #define PN532_SS1  15
-// #define PN532_SS2   4
+// #define PN532_SS   4
 
 #endif // CONFIG_H
