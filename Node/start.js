bd90b91fa9f3844ca7b1907b1012c127dba6e103
Cooper Newby <newbycooper@gmail.com>
2022-11-23 17:53:49 -0800

    Update start

--- Node/start.js
+++ Node/start.js
@@ -22,7 +22,7 @@ setInterval(() => {
  
 setInterval(() => {
    sockserver.clients.forEach((client) => {
-       const messages = ['Hello', 'What do you ponder?', 'Thank you for your time', 'Be Mindful', 'Thank You'];
+       const messages = ['ESP1', 'ESP2', 'ESP3', 'ESP4', 'ESP5'];
        const random = Math.floor(Math.random() * messages.length);
        let position = {x: Math.floor(Math.random() * 200), y: Math.floor(Math.random() * 150)}
        const data = JSON.stringify({'type': 'message', 'message': messages[random], 'position': position});
