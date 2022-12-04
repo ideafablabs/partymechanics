0b6b9578980b44574bd7b0fa0964250fbde00576
Cooper Newby <newbycooper@gmail.com>
2022-12-03 19:03:49 -0800

    switch to SSL sockets

--- Arduino/websockets/websockets.ino
+++ Arduino/websockets/websockets.ino
@@ -1,19 +1,10 @@
-/*
- * IFL Zone +1 Token Registration Code
- * Send the token ID from an NFC or RFID card up to an api endpoint.
- *
- * @contributors: Jordan Layman, David Van Brink, John Szymanski, Geoff Gnau, Tané Tachyon, Corey Brown
- */
-
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_PN532.h>
-#include <ArduinoOTA.h>
-
-// Onboard Libs
+#include <ArduinoWebsockets.h>
 #include <ESPAsyncWebServer.h>
 #include <asyncHTTPrequest.h>
-#include <WebSocketsClient.h>
+#include <WiFi.h>
+// Communications
+asyncHTTPrequest apiClient;
+AsyncWebServer server(80);
 
 #ifdef ESP32
 #include <WiFiMulti.h>
@@ -23,443 +14,141 @@
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266mDNS.h>
 #include <FS.h>
-
 #endif
 
-// Setup Config.h by duplicating config-sample.h.
-#include "config.h"
+#define SSID1 "Idea Fab Labs"
+#define PASSWORD1 "vortexrings"
 
-// Time
-long now = 0;
+// https://pusher.com/
+//[scheme]://ws-[cluster_name].pusher.com:[port]/app/[key]
+// ws - for a normal WebSocket connection
+// wss - for a secure WebSocket connection
+// cluster_name - The name of the cluster that you’re using
+// port - Default WebSocket ports: 80 (ws) or 443 (wss)
+// key - The app key for the application connecting to Pusher Channels
+const char *websockets_connection_string = "wss://ws-us3.pusher.com:443/app/01a52d68bccce5e260bd"; // Enter server adress
 
-//  NFC
-Adafruit_PN532 nfcs[READER_COUNT] = {
-    Adafruit_PN532(PN532_SS1)
-    // Adafruit_PN532(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS2),
-    // Adafruit_PN532(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS3),
-    // Adafruit_PN532(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS4),
-};
+const char pusher_ssl_ca_cert[] PROGMEM =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIGLjCCBRagAwIBAgIQdATkQFYKykh1q4tPMVCRjDANBgkqhkiG9w0BAQsFADBf\n"
+    "MQswCQYDVQQGEwJGUjEOMAwGA1UECBMFUGFyaXMxDjAMBgNVBAcTBVBhcmlzMQ4w\n"
+    "DAYDVQQKEwVHYW5kaTEgMB4GA1UEAxMXR2FuZGkgU3RhbmRhcmQgU1NMIENBIDIw\n"
+    "HhcNMjIwNDA3MDAwMDAwWhcNMjMwNDIxMjM1OTU5WjAXMRUwEwYDVQQDDAwqLnB1\n"
+    "c2hlci5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDXMYx+u6oW\n"
+    "XbfBIbInBq1f7TkKRfxbw9oZHNKx7VeukHrhTHXh438zzyRCABHVR+CX91D8Ork9\n"
+    "V86mOVwRBFF05VoweX7EjFhirz3l6XTbPUUR5XyIwKNxYTXovxNkK7ahVbnA1+et\n"
+    "SAnFyJxuhEz8zKcm0UAl7JzghRNb73SVhePmr95xyno58sTckqKwDCx202ebelbB\n"
+    "IfhX19vt2KA3VbCapc8f1vjtunnr5yIldYU8jsjMWZxZSneg8AVHaRRToh5ngIch\n"
+    "uXfWhH2HolzrXfrASdLvnmL93qSdRkhAthPBg33e+72KF2KecYrDJTrPJ/2rmJdZ\n"
+    "yw5AhrO1nz4fAgMBAAGjggMsMIIDKDAfBgNVHSMEGDAWgBSzkKfYya9OzWE8n3yt\n"
+    "XX9B/Wkw6jAdBgNVHQ4EFgQUD7PZ/LHJRRk8TBp5ZmYKAEzWjIkwDgYDVR0PAQH/\n"
+    "BAQDAgWgMAwGA1UdEwEB/wQCMAAwHQYDVR0lBBYwFAYIKwYBBQUHAwEGCCsGAQUF\n"
+    "BwMCMEsGA1UdIAREMEIwNgYLKwYBBAGyMQECAhowJzAlBggrBgEFBQcCARYZaHR0\n"
+    "cHM6Ly9jcHMudXNlcnRydXN0LmNvbTAIBgZngQwBAgEwQQYDVR0fBDowODA2oDSg\n"
+    "MoYwaHR0cDovL2NybC51c2VydHJ1c3QuY29tL0dhbmRpU3RhbmRhcmRTU0xDQTIu\n"
+    "Y3JsMHMGCCsGAQUFBwEBBGcwZTA8BggrBgEFBQcwAoYwaHR0cDovL2NydC51c2Vy\n"
+    "dHJ1c3QuY29tL0dhbmRpU3RhbmRhcmRTU0xDQTIuY3J0MCUGCCsGAQUFBzABhhlo\n"
+    "dHRwOi8vb2NzcC51c2VydHJ1c3QuY29tMCMGA1UdEQQcMBqCDCoucHVzaGVyLmNv\n"
+    "bYIKcHVzaGVyLmNvbTCCAX0GCisGAQQB1nkCBAIEggFtBIIBaQFnAHUArfe++nz/\n"
+    "EMiLnT2cHj4YarRnKV3PsQwkyoWGNOvcgooAAAGAA3xK2wAABAMARjBEAiALNO0C\n"
+    "nxK5O7PVwrJ0bvX964Hf5wmhDt0jPXLSYyRscAIgSc3L+mr5CdvhVAM4tJGYmKPV\n"
+    "euKMTuhtPeRZVtwzIPMAdwB6MoxU2LcttiDqOOBSHumEFnAyE4VNO9IrwTpXo1Lr\n"
+    "UgAAAYADfErZAAAEAwBIMEYCIQCECmH3cn5NeOL+D10AEjbDjBg/GFb+oDKTPEwa\n"
+    "8NdLCAIhAPOGps4v/OFjPi0d971wydiZgfOGqk7xIreiVRDPP+WZAHUA6D7Q2j71\n"
+    "BjUy51covIlryQPTy9ERa+zraeF3fW0GvW4AAAGAA3xKvQAABAMARjBEAiAiHqWD\n"
+    "WD4MNOC83pcGFss1rGIrA1XoIk8V5dkNlCgR3gIgCNOzBEOYUmG7JxcoK2K/ZMq7\n"
+    "LhLio5kZwgFFVJSDzGEwDQYJKoZIhvcNAQELBQADggEBACFAw+gkHM/P0dGu+3T5\n"
+    "68hzmlzTB6gAvVEYP4r89Xk+bRF0iydZ+5gRjhQOecne+VGFoITdIePJy2aqMbaW\n"
+    "+xfNO2c5wWpTuXxK6INmjHs5UexKTbA3g9jjI1R5LDQF3yZkuFFsGvvaLlycZSNX\n"
+    "DLr8iA37lIjSsYVwJo9KUXfFFfyZ6zok/r5MeO658MTxCcGsF7pQF9svie2y8LbC\n"
+    "x63CXOuf7wjdv0onfMFwMugO7yrA7uyreQsmdTOiSl7PieJsW28BylNPaAYPxatm\n"
+    "nChpyGQqoS715avbaEBuhdxrOFROMeIOJRG2K2l2H9cx3958CVNns0lxKz9r5oyD\n"
+    "8is=\n"
+    "-----END CERTIFICATE-----\n";
 
-typedef uint32_t nfcid_t; // We treat the NFCs as 4 byte values throughout, for easiest.
-long lastRead, successTime = 0;
-uint16_t cardreaderPeriod = 500; // ms
-uint16_t successPeriod = 3000;   // ms
+using namespace websockets;
 
-enum readerState
+void onMessageCallback(WebsocketsMessage message)
 {
-  RQ_STANDBY,
-  RQ_PENDING,
-  RQ_SUCCESS,
-  RQ_FAILED
-};
-uint8_t state = RQ_STANDBY;
+    Serial.print("Got Message: ");
+    Serial.println(message.data());
+}
 
-// LED
-// Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LEDPIN, NEO_GRB + NEO_KHZ800);
-CRGB leds[NUM_LEDS];
-
-int step = 0;
-long lastBlink = 0;
-const uint16_t ledPeriod = 40; // ms 24fps
-uint32_t tokenColors[] = {
-    0x00FF00,
-    0xFFFF00,
-    0xFF0000,
-    0x0000FF,
-    0xFF00FF,
-    0xFFFFFF,
-}; // Should be G Y R B (& secret purple)
-
-// uint8_t tokenCount = 0;
-// uint8_t tokenSelection[READER_COUNT];
-// uint32_t tokensAchieved[READER_COUNT];
-
-long holdTimes[READER_COUNT];
-long holdStartTimes[READER_COUNT];
-
-// nfcid_t tokensAchieved
-nfcid_t lastIDs[READER_COUNT];
-nfcid_t tokenIDs[READER_COUNT];
-
-enum mapping
+void onEventsCallback(WebsocketsEvent event, String data)
 {
-  SOCKETSTART1 = 0,
-  SOCKETEND1 = 0,
-  SOCKETSTART2 = 1,
-  SOCKETEND2 = 1,
-  SOCKETSTART3 = 2,
-  SOCKETEND3 = 2,
-  SOCKETSTART4 = 3,
-  SOCKETEND4 = 3,
-  METERSTART1 = 4,
-  METEREND1 = 7,
-  METERSTART2 = 8,
-  METEREND2 = 11
-};
-
-// Communications
+    if (event == WebsocketsEvent::ConnectionOpened)
+    {
+        Serial.println("Connnection Opened");
+    }
+    else if (event == WebsocketsEvent::ConnectionClosed)
+    {
+        Serial.println("Connnection Closed");
+    }
+    else if (event == WebsocketsEvent::GotPing)
+    {
+        Serial.println("Got a Ping!");
+    }
+    else if (event == WebsocketsEvent::GotPong)
+    {
+        Serial.println("Got a Pong!");
+    }
+}
+#ifdef ESP32
 WiFiMulti wifiMulti;
-asyncHTTPrequest apiClient;
-AsyncWebServer server(80);
-WebSocketsClient webSocket;
-uint8_t socketState;
-
+#else
+ESP8266WiFiMulti wifiMulti;
+#endif
+WebsocketsClient client;
 void setup()
 {
+    Serial.begin(115200);
 
-  Serial.begin(115200);
-  Serial.println("Henlo!");
+    WiFi.mode(WIFI_STA);
+    wifiMulti.addAP(SSID1, PASSWORD1);
+    // Wait some time to connect to wifi
+    Serial.print("Wifi Connecting.");
+    while (wifiMulti.run() == WL_CONNECTED)
+    {
+        Serial.println("");
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+    }
+    //    while (wifiMulti.run() != WL_CONNECTED)
+    //    {
+    //      Serial.print(".");
+    //      delay(1000);
+    //    }
+    //      Serial.println("Connecting Wifi...");
 
-  setupWiFi();
+    client.setCACert(pusher_ssl_ca_cert);
 
-  setupClient();
+    Serial.println("Connected to Wifi, Connecting to server.");
+    // try to connect to Websockets server
+    bool connected = client.connect(websockets_connection_string);
+    if (connected)
+    {
+        Serial.println("Connected!");
+        client.send("Hello Server");
+    }
+    else
+    {
+        Serial.println("Not Connected!");
+    }
 
-  logAction("Booted Up");
-
-  // printLog();
+    // run callback when messages are received
+    client.onMessage([&](WebsocketsMessage message)
+                     {
+        Serial.print("Got Message: ");
+        Serial.println(message.data()); });
 }
 
 void loop()
 {
-
-  // Get time.
-  now = millis();
-  Serial.println("boom!");
-  webSocket.loop();
-}
-
-void webSocketEvent(WStype_t type, uint8_t *payload, size_t lenght)
-{
-
-  switch (type)
-  {
-  case WStype_DISCONNECTED:
-    USE_SERIAL.printf("[WSc] Disconnected!\n");
-    break;
-  case WStype_CONNECTED:
-  {
-    USE_SERIAL.printf("[WSc] Connected to url: %s\n", payload);
-
-    // send message to server when Connected
-    webSocket.sendTXT("Connected");
-  }
-  break;
-  case WStype_TEXT:
-    USE_SERIAL.printf("[WSc] get text: %s\n", payload);
-
-    // send message to server
-    // webSocket.sendTXT("message here");
-    break;
-  case WStype_BIN:
-    USE_SERIAL.printf("[WSc] get binary lenght: %u\n", lenght);
-    // hexdump(payload, lenght);
-
-    // send data to server
-    // webSocket.sendBIN(payload, lenght);
-    break;
-  }
-}
-
-// we think idcode is always even...
-// this is mostly because we read the little-endian id as if it were
-// big-endian and are getting kinda lucky. But this /2 mod5 thing works so ok for now. dvb 2019.
-uint8_t getTokenColor(nfcid_t uid)
-{
-  uid /= 2;
-  int flavor = uid % 5;
-  return flavor;
-}
-
-// Helpers to extract RGB from 32bit color.
-uint8_t extractRed(uint32_t c) { return ((c >> 16) & 0xFF); }
-uint8_t extractGreen(uint32_t c) { return ((c >> 8) & 0xFF); }
-uint8_t extractBlue(uint32_t c) { return (c & 0xFF); }
-
-uint32_t rgba(byte r, byte g, byte b, int a)
-{
-
-  int rr = (r * a) / 100;
-  int gg = (g * a) / 100;
-  int bb = (b * a) / 100;
-
-  uint32_t hex = (rr << 16L) | (gg << 8L) | bb;
-
-  return hex;
-}
-
-uint32_t alpha(uint32_t c, int a)
-{
-
-  uint8_t r = extractRed(c);
-  uint8_t g = extractGreen(c);
-  uint8_t b = extractBlue(c);
-
-  return rgba(r, g, b, a);
-}
-
-// https://github.com/boblemaire/asyncHTTPrequest
-// https://stackoverflow.com/questions/54820798/how-to-receive-json-response-from-rest-api-using-esp8266-arduino-framework
-void onClientStateChange(void *arguments, asyncHTTPrequest *aReq, int readyState)
-{
-
-  switch (readyState)
-  {
-  case 0: // readyStateUnsent: Client created, open not yet called.
-    break;
-
-  case 1: // readyStateOpened: open() has been called, connected
-    break;
-
-  case 2: // readyStateHdrsRecvd: send() called, response headers available
-    break;
-
-  case 3: // readyStateLoading: receiving, partial data available
-    break;
-
-  case 4: // readyStateDone: Request complete, all data available.
-
-    // Log Response.
-    /// We might want to store the response and check syncronously so log doesn't get chunked.
-    String rcode = (String)aReq->responseHTTPcode();
-    logAction(rcode + " " + aReq->responseText());
-
-    // request case here.
-    if (aReq->responseHTTPcode() == 200)
+    // let the websockets client check for incoming messages
+    if (client.available())
     {
-      state = RQ_SUCCESS;
-      successTime = now;
+        client.poll();
     }
-    else
-    {
-      state = RQ_FAILED;
-      successTime = now;
-    }
-
-    /// This should probably be json so we can confirm respones types.
-
-    break;
-  }
-}
-
-// Wifi Setup.
-void setupWiFi()
-{
-  WiFi.mode(WIFI_STA);
-
-  wifiMulti.addAP(SSID1, PASSWORD1);
-  wifiMulti.addAP(SSID2, PASSWORD2);
-
-  Serial.print("Wifi Connecting.");
-  while (wifiMulti.run() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(1000);
-  }
-
-  logAction("WiFi connected to SSID: '" + WiFi.SSID() + "' @ " + WiFi.localIP().toString());
-}
-
-void startAsyncRequest(String request, String params, String type)
-{
-
-  logAction(type + " REQUEST: " + request + "?" + params);
-
-  if (apiClient.readyState() == 0 || apiClient.readyState() == 4)
-  { // Only one send at a time here.
-    apiClient.open(type.c_str(), request.c_str());
-    if (type == "POST")
-      apiClient.setReqHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    apiClient.send(params);
-  }
-  else
-  {
-    logAction("Request attempted but busy sending...Try again.");
-  }
-}
-
-const char *PARAM_MESSAGE = "message"; ///
-void setupServer()
-{
-
-  // Start the file system.
-  if (!SPIFFS.begin())
-  {
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
-
-  // Root / Home
-  // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-  //      request->send(200, "text/plain", printLog());
-  //  });
-
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
-    request->send(200, "text/plain", "fgkldsjklfgjklgfdljk");
-
-    // request->send(SPIFFS, LOG_FILE, "text/plain"); });
-
-    server.serveStatic("/log/", SPIFFS, LOG_FILE);
-
-    AsyncStaticWebHandler &serveStatic(const char *uri, fs::FS &fs, const char *path, const char *cache_control = NULL);
-
-    // server.on("/log/flush", HTTP_GET, [](AsyncWebServerRequest *request){
-    //  flushLog();
-    //  request->send(200, "text/plain", "Log flushed.");
-    //  // request->send(SPIFFS, "/"+LOG_FILE, "text/plain");
-    // });
-
-    // // DEMO
-    // // Send a GET request to <IP>/get?message=<message>
-    // server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    //     String message;
-    //     if (request->hasParam(PARAM_MESSAGE)) {
-    //         message = request->getParam(PARAM_MESSAGE)->value();
-    //     } else {
-    //         message = "No message sent";
-    //     }
-    //     request->send(200, "text/plain", "Hello, GET: " + message);
-    // });
-
-    // // DEMO
-    //   // Send a POST request to <IP>/post with a form field message set to <message>
-    //   server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
-    //       String message;
-    //       if (request->hasParam(PARAM_MESSAGE, true)) {
-    //           message = request->getParam(PARAM_MESSAGE, true)->value();
-    //       } else {
-    //           message = "No message sent";
-    //       }
-    //       request->send(200, "text/plain", "Hello, POST: " + message);
-    //   });
-
-    server.onNotFound(notFound);
-
-    server.begin();
-}
-void notFound(AsyncWebServerRequest *request)
-{
-    request->send(404, "text/plain", "Connected but not found");
-}
-
-// Return the 64 bit uid, with ZERO meaning nothing presently detected.
-nfcid_t pollNfc(uint8_t reader_id)
-{
-    uint8_t uidBytes[8] = {0};
-    uint8_t uidLength;
-    nfcid_t uid = 0;
-
-    // static int pollCount = 0; // just for printing the poll dots.
-    // char pollChar = '.'; // dots for no read, + for active.
-
-    // Check for card
-    int foundCard = nfcs[reader_id].readPassiveTargetID(PN532_MIFARE_ISO14443A, &uidBytes[0], &uidLength, 100);
-
-    if (foundCard)
-    {
-      uidLength = 4; // it's little endian, the lower four are enough, and all we can use on this itty bitty cpu. ///magic numbers
-
-      // Unspool the bins right here.
-      for (int ix = 0; ix < uidLength; ix++)
-        uid = (uid << 8) | uidBytes[ix];
-
-      // pollChar = '+'; //
-    }
-
-    // if (pollCount % 20 == 0)  // so the dots dont scroll right forever.
-    //  Serial.printf("\n%4d ", pollCount);
-    // pollCount++;
-    // Serial.print(pollChar);
-    return uid;
-}
-
-// Count Macro.
-#define count(x) (sizeof(x) / sizeof(x[0]))
-
-// Show real number for tokenID.
-long id(uint8_t bins[])
-{
-    uint32_t c;
-    c = bins[0];
-    for (int i = 1; i < count(bins); i++)
-    {
-      c <<= 8;
-      c |= bins[i];
-    }
-    return c;
-}
-
-// === Log Functions ===
-String printLog()
-{
-    int xCnt = 0;
-    String output;
-
-    File f = SPIFFS.open(LOG_FILE, "r");
-
-    if (!f)
-    {
-      output = "file open failed";
-      return output;
-    }
-
-    output = "====== Reading from LOG_FILE =======";
-
-    while (f.available())
-    {
-      // Lets read line by line from the file
-      String line = f.readStringUntil('\n');
-      // Serial.print(xCnt);
-      // Serial.print("  ");
-      // Serial.println(line);
-      output += xCnt + "  " + line + "\n";
-
-      xCnt++;
-    }
-    f.close();
-
-    return output;
-}
-
-void listFiles()
-{
-    File root = SPIFFS.open("/");
-    File file = root.openNextFile();
-
-    Serial.println("Listing SPIFFS Files");
-
-    while (file)
-    {
-
-      Serial.print("FILE: ");
-      Serial.println(file.name());
-
-      file = root.openNextFile();
-    }
-}
-
-void flushLog()
-{
-    File f = SPIFFS.open(LOG_FILE, "w");
-    f.printf("%s %s: ", __DATE__, __TIME__);
-    f.println("Begin Log");
-    f.close();
-}
-
-void logAction(String actionString)
-{
-    Serial.printf("\n%s %s: ", __DATE__, __TIME__);
-    Serial.println(actionString);
-
-    char *mode = "a";
-
-    // if not exists, create using W, other wise append with A
-    if (!SPIFFS.exists(LOG_FILE))
-      mode = "w";
-
-    File f = SPIFFS.open(LOG_FILE, mode);
-    f.printf("%s %s: ", __DATE__, __TIME__);
-    f.println(actionString);
-    f.close();
+    delay(500);
 }
